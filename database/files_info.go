90da480f7559d1fa64eabd95915cf17a52a8e917
Dmytro Galushko <galushko@netcracker.com>
2025-01-07 17:23:20 +0200

    Adapt to 1.16.2 Core

--- database/files_info.go
+++ database/files_info.go
@@ -6,28 +6,28 @@ func AddFileLocation(id int64, path string, name string, length float64) error {
 	query := `INSERT INTO files_info (id, path, name, length)
 				VALUES (?, ?, ?, ?)
 				ON CONFLICT(id) DO UPDATE SET
-					path = excluded.path
-				    name = excluded.name
-				    length = excluded.length;`
+					path = excluded.path,
+					name = excluded.name,
+					length = excluded.length;`
 	_, err := DB.Exec(query, id, path, name, length)
 	if err != nil {
 		log.Printf("[AddFileLocation] Помилка при додаванні запису: %v", err)
 		return err
 	}
 
-	log.Printf("[AddFileLocation] Створено новий запис: ID %d для Name %s , Path: %s розміром %f", id, name, path, length)
+	log.Printf("[AddFileLocation] Створено або оновлено запис: ID %d для Name %s, Path: %s розміром %f", id, name, path, length)
 	return nil
 }
 
 func GetFileInfo(id int64) (string, string, float64, error) {
-	query := `SELECT path FROM files_info WHERE id = ?;`
+	query := `SELECT path, name, length FROM files_info WHERE id = ?;`
 	row := DB.QueryRow(query, id)
 
 	var path string
 	var name string
 	var length float64
 
-	err := row.Scan(&path, &name)
+	err := row.Scan(&path, &name, &length)
 	if err == nil {
 		log.Printf("[GetFileLocation] Знайдено існуючий ID: %d зі шляхом: %s на ім'я: %s розміром: %f", id, path, name, length)
 		return path, name, length, nil
